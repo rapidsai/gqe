72498e35728d2a8f4ff2b4a0b4c3c3b4883cad0b
Daniel Juenger <12551-djuenger@users.noreply.gitlab-master.nvidia.com>
2023-06-08 12:49:25 -0700

    Add TPC-DS utilities (Devtech-Compute/gqe!94)
    This MR adds utilities such as DDL/table definitions and data types for TPC-DS.

--- benchmark/tpcds.cpp
+++ benchmark/tpcds.cpp
@@ -15,6 +15,7 @@
 #include <gqe/logical/from_substrait.hpp>
 #include <gqe/optimizer/physical_transformation.hpp>
 #include <gqe/utility/helpers.hpp>
+#include <gqe/utility/tpcds.hpp>
 
 #include <cudf/io/parquet.hpp>
 
@@ -48,209 +49,16 @@ int main(int argc, char* argv[])
   rmm::mr::pool_memory_resource<rmm::mr::cuda_memory_resource> pool_mr{&cuda_mr};
   rmm::mr::set_current_device_resource(&pool_mr);
 
-  // Register the input tables
-  auto const identifier_type = cudf::data_type(cudf::type_id::INT64);
-  auto const integer_type    = cudf::data_type(cudf::type_id::INT64);
-  auto const decimal_type    = cudf::data_type(cudf::type_id::FLOAT64);
-  auto const string_type     = cudf::data_type(cudf::type_id::STRING);
-  auto const date_type       = cudf::data_type(cudf::type_id::TIMESTAMP_DAYS);
-
   gqe::catalog tpcds_catalog;
-  tpcds_catalog.register_table("store_sales",
-                               {{"ss_sold_date_sk", identifier_type},
-                                {"ss_sold_time_sk", identifier_type},
-                                {"ss_item_sk", identifier_type},
-                                {"ss_customer_sk", identifier_type},
-                                {"ss_cdemo_sk", identifier_type},
-                                {"ss_hdemo_sk", identifier_type},
-                                {"ss_addr_sk", identifier_type},
-                                {"ss_store_sk", identifier_type},
-                                {"ss_promo_sk", identifier_type},
-                                {"ss_ticket_number", identifier_type},
-                                {"ss_quantity", integer_type},
-                                {"ss_wholesale_cost", decimal_type},
-                                {"ss_list_price", decimal_type},
-                                {"ss_sales_price", decimal_type},
-                                {"ss_ext_discount_amt", decimal_type},
-                                {"ss_ext_sales_price", decimal_type},
-                                {"ss_ext_wholesale_cost", decimal_type},
-                                {"ss_ext_list_price", decimal_type},
-                                {"ss_ext_tax", decimal_type},
-                                {"ss_coupon_amt", decimal_type},
-                                {"ss_net_paid", decimal_type},
-                                {"ss_net_paid_inc_tax", decimal_type},
-                                {"ss_net_profit", decimal_type}},
-                               gqe::storage_kind::parquet_file{gqe::utility::get_parquet_files(
-                                 dataset_location + "/store_sales")},
-                               gqe::partitioning_schema_kind::automatic{});
 
-  tpcds_catalog.register_table("date_dim",
-                               {{"d_date_sk", identifier_type},
-                                {"d_date_id", string_type},
-                                {"d_date", date_type},
-                                {"d_month_seq", integer_type},
-                                {"d_week_seq", integer_type},
-                                {"d_quarter_seq", integer_type},
-                                {"d_year", integer_type},
-                                {"d_dow", integer_type},
-                                {"d_moy", integer_type},
-                                {"d_dom", integer_type},
-                                {"d_qoy", integer_type},
-                                {"d_fy_year", integer_type},
-                                {"d_fy_quarter_seq", integer_type},
-                                {"d_fy_week_seq", integer_type},
-                                {"d_day_name", string_type},
-                                {"d_quarter_name", string_type},
-                                {"d_holiday", string_type},
-                                {"d_weekend", string_type},
-                                {"d_following_holiday", string_type},
-                                {"d_first_dom", integer_type},
-                                {"d_last_dom", integer_type},
-                                {"d_same_day_ly", integer_type},
-                                {"d_same_day_lq", integer_type},
-                                {"d_current_day", string_type},
-                                {"d_current_week", string_type},
-                                {"d_current_month", string_type},
-                                {"d_current_quarter", string_type},
-                                {"d_current_year", string_type}},
-                               gqe::storage_kind::parquet_file{
-                                 gqe::utility::get_parquet_files(dataset_location + "/date_dim")},
-                               gqe::partitioning_schema_kind::automatic{});
-
-  tpcds_catalog.register_table(
-    "item",
-    {{"i_item_sk", identifier_type},     {"i_item_id", string_type},
-     {"i_rec_start_date", date_type},    {"i_rec_end_date", date_type},
-     {"i_item_desc", string_type},       {"i_current_price", decimal_type},
-     {"i_wholesale_cost", decimal_type}, {"i_brand_id", integer_type},
-     {"i_brand", string_type},           {"i_class_id", integer_type},
-     {"i_class", string_type},           {"i_category_id", integer_type},
-     {"i_category", string_type},        {"i_manufact_id", integer_type},
-     {"i_manufact", string_type},        {"i_size", string_type},
-     {"i_formulation", string_type},     {"i_color", string_type},
-     {"i_units", string_type},           {"i_container", string_type},
-     {"i_manager_id", integer_type},     {"i_product_name", string_type}},
-    gqe::storage_kind::parquet_file{gqe::utility::get_parquet_files(dataset_location + "/item")},
-    gqe::partitioning_schema_kind::automatic{});
-
-  tpcds_catalog.register_table("customer_demographics",
-                               {{"cd_demo_sk", identifier_type},
-                                {"cd_gender", string_type},
-                                {"cd_marital_status", string_type},
-                                {"cd_education_status", string_type},
-                                {"cd_purchase_estimate", integer_type},
-                                {"cd_credit_rating", string_type},
-                                {"cd_dep_count", integer_type},
-                                {"cd_dep_employed_count", integer_type},
-                                {"cd_dep_college_count", integer_type}},
-                               gqe::storage_kind::parquet_file{gqe::utility::get_parquet_files(
-                                 dataset_location + "/customer_demographics")},
-                               gqe::partitioning_schema_kind::automatic{});
-
-  tpcds_catalog.register_table("promotion",
-                               {{"p_promo_sk", identifier_type},
-                                {"p_promo_id", string_type},
-                                {"p_start_date_sk", identifier_type},
-                                {"p_end_date_sk", identifier_type},
-                                {"p_item_sk", identifier_type},
-                                {"p_cost", decimal_type},
-                                {"p_response_target", integer_type},
-                                {"p_promo_name", string_type},
-                                {"p_channel_dmail", string_type},
-                                {"p_channel_email", string_type},
-                                {"p_channel_catalog", string_type},
-                                {"p_channel_tv", string_type},
-                                {"p_channel_radio", string_type},
-                                {"p_channel_press", string_type},
-                                {"p_channel_event", string_type},
-                                {"p_channel_demo", string_type},
-                                {"p_channel_details", string_type},
-                                {"p_purpose", string_type},
-                                {"p_discount_active", string_type}},
-                               gqe::storage_kind::parquet_file{
-                                 gqe::utility::get_parquet_files(dataset_location + "/promotion")},
-                               gqe::partitioning_schema_kind::automatic{});
-
-  tpcds_catalog.register_table("web_sales",
-                               {{"ws_sold_date_sk", identifier_type},
-                                {"ws_item_sk", identifier_type},
-                                {"ws_ext_sales_price", decimal_type}},
-                               gqe::utility::get_parquet_files(dataset_location + "/web_sales"),
-                               gqe::file_format_type::parquet);
-
-  tpcds_catalog.register_table(
-    "store",
-    {{"s_store_sk", identifier_type},
-     {"s_store_id", string_type},
-     {"s_rec_start_date", date_type},
-     {"s_rec_end_date", date_type},
-     {"s_closed_date_sk", identifier_type},
-     {"s_store_name", string_type},
-     {"s_number_employees", integer_type},
-     {"s_floor_space", integer_type},
-     {"s_hours", string_type},
-     {"s_manager", string_type},
-     {"s_market_id", integer_type},
-     {"s_geography_class", string_type},
-     {"s_market_desc", string_type},
-     {"s_market_manager", string_type},
-     {"s_division_id", integer_type},
-     {"s_division_name", string_type},
-     {"s_company_id", integer_type},
-     {"s_company_name", string_type},
-     {"s_street_number", string_type},
-     {"s_street_name", string_type},
-     {"s_street_type", string_type},
-     {"s_suite_number", string_type},
-     {"s_city", string_type},
-     {"s_county", string_type},
-     {"s_state", string_type},
-     {"s_zip", string_type},
-     {"s_country", string_type},
-     {"s_gmt_offset", decimal_type},
-     {"s_tax_percentage", decimal_type}},
-    gqe::storage_kind::parquet_file{gqe::utility::get_parquet_files(dataset_location + "/store")},
-    gqe::partitioning_schema_kind::automatic{});
-
-  tpcds_catalog.register_table("customer_address",
-                               {{"ca_address_sk", identifier_type},
-                                {"ca_address_id", string_type},
-                                {"ca_street_number", string_type},
-                                {"ca_street_name", string_type},
-                                {"ca_street_type", string_type},
-                                {"ca_suite_number", string_type},
-                                {"ca_city", string_type},
-                                {"ca_county", string_type},
-                                {"ca_state", string_type},
-                                {"ca_zip", string_type},
-                                {"ca_country", string_type},
-                                {"ca_gmt_offset", decimal_type},
-                                {"ca_location_type", string_type}},
-                               gqe::storage_kind::parquet_file{gqe::utility::get_parquet_files(
-                                 dataset_location + "/customer_address")},
-                               gqe::partitioning_schema_kind::automatic{});
-
-  tpcds_catalog.register_table("customer",
-                               {{"c_customer_sk", cudf::data_type(cudf::type_id::INT64)},
-                                {"c_last_name", cudf::data_type(cudf::type_id::STRING)},
-                                {"c_first_name", cudf::data_type(cudf::type_id::STRING)},
-                                {"c_current_addr_sk", cudf::data_type(cudf::type_id::INT64)}},
-                               gqe::storage_kind::parquet_file{
-                                 gqe::utility::get_parquet_files(dataset_location + "/customer")},
-                               gqe::partitioning_schema_kind::automatic{});
-  tpcds_catalog.register_table("catalog_sales",
-                               {{"cs_sold_date_sk", cudf::data_type(cudf::type_id::INT64)},
-                                {"cs_bill_customer_sk", cudf::data_type(cudf::type_id::INT64)}},
-                               gqe::storage_kind::parquet_file{gqe::utility::get_parquet_files(
-                                 dataset_location + "/catalog_sales")},
-                               gqe::partitioning_schema_kind::automatic{});
-
-  tpcds_catalog.register_table("web_sales",
-                               {{"ws_sold_date_sk", cudf::data_type(cudf::type_id::INT64)},
-                                {"ws_bill_customer_sk", cudf::data_type(cudf::type_id::INT64)}},
-                               gqe::storage_kind::parquet_file{
-                                 gqe::utility::get_parquet_files(dataset_location + "/web_sales")},
-                               gqe::partitioning_schema_kind::automatic{});
+  // register all tables
+  for (auto const& [name, definition] : gqe::utility::tpcds::table_definitions()) {
+    tpcds_catalog.register_table(name,
+                                 definition,
+                                 gqe::storage_kind::parquet_file{
+                                   gqe::utility::get_parquet_files(dataset_location + "/" + name)},
+                                 gqe::partitioning_schema_kind::automatic{});
+  }
 
   gqe::substrait_parser parser(&tpcds_catalog);
   auto logical_plan = parser.from_file(substrait_plan_file);
---
+++ include/gqe/utility/tpcds.hpp
@@ -0,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+ *
+ * NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
+ * property and proprietary rights in and to this material, related
+ * documentation and any modifications thereto. Any use, reproduction,
+ * disclosure or distribution of this material and related documentation
+ * without an express license agreement from NVIDIA CORPORATION or
+ * its affiliates is strictly prohibited.
+ */
+
+#pragma once
+
+#include <gqe/types.hpp>
+
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+
+namespace gqe::utility {
+namespace tpcds {
+
+/**
+ * @brief Type mappings between TPC-DS and GQE
+ */
+constexpr auto identifier_type = cudf::data_type(cudf::type_id::INT64);
+constexpr auto integer_type    = cudf::data_type(cudf::type_id::INT64);
+constexpr auto decimal_type    = cudf::data_type(cudf::type_id::FLOAT64);
+constexpr auto string_type     = cudf::data_type(cudf::type_id::STRING);
+constexpr auto date_type       = cudf::data_type(cudf::type_id::TIMESTAMP_DAYS);
+
+/**
+ * @brief Helper type to map column names to data types
+ */
+using column_definition_type = std::pair<std::string, cudf::data_type>;
+
+/**
+ * @brief Map of the TPC-DS DDL
+ *
+ * @details See the [TPC-DS
+ * reference](https://www.tpc.org/tpc_documents_current_versions/pdf/tpc-ds_v3.2.0.pdf) for details.
+ *
+ * @return A map, which associates table names to column definition vectors
+ */
+std::unordered_map<std::string, std::vector<column_definition_type>> const&
+table_definitions() noexcept;
+
+}  // namespace tpcds
+}  // namespace gqe::utility
